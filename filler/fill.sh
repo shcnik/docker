76f666761c53c12faa35779113d6690bec8e7e04
shcnik <shcherbakov.nd@phystech.edu>
2023-11-15 15:50:07 +0300

    Correction

--- filler/fill.sh
+++ filler/fill.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-mysql -h db -u shcnik -pdisco_sovenok data -e 'CREATE OR REPLACE TABLE data ( id INT NOT NULL AUTO_INCREMENT, name TINYTEXT, age TINYINT, 
-PRIMARY KEY (id) )'
+mysql -h db -u $3 -p$4 $1 -e "CREATE OR REPLACE TABLE $2 ( id INT NOT NULL AUTO_INCREMENT, name TINYTEXT, age TINYINT, PRIMARY KEY (id) )"
 skip_headers=1
 while IFS=, read -r name age
 do
@@ -8,7 +7,7 @@ do
     then
         ((skip_headers--))
     else
-        mysql -h db -u shcnik -u root -pdisco_sovenok data -e "INSERT INTO data (name, age) VALUES ('$name', '$age')"
+        mysql -h db -u $3 -p$4 $1 -e "INSERT INTO $2 (name, age) VALUES ('$name', '$age')"
     fi
 done < contents/data.csv
 
